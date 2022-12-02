8c4e83c74e954f7faa6268c88b0d5e5703ff8997
Tolani <tolani@Administrators-MacBook-Pro.local>
2022-12-02 13:57:12 +0100

    style result lists

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,40 +1,30 @@
 import PropTypes from 'prop-types';
-import { Pagination } from '@mui/material';
-import { useState } from 'react';
 import formatDate from '../helpers/date';
 import '../styles/results.scss';
+// import Pagination from './Pagination';
 
 const Results = ({ results }) => {
   console.log('results', results);
-  const [page, setPage] = useState(0);
-
   return (
     <div className="search-results">
       <p className="search-results__title">Search results</p>
 
-      {results.length > 0 ? (
-        <Pagination
-          count={results.length}
-          page={page}
-          hidePrevButton
-          onChange={(newPage) => setPage(newPage)}
-        />
-      ) : (
-        ''
-      )}
+      {/* <Pagination results={results} /> */}
 
       <div className="search-box">
         {results ? (
           results.map((result, index) => (
             <div key={index.id} className="single-result">
               <h4>{result.title}</h4>
-              <div>
+              <div className="single-result__author">
                 <span>{result.authorString}</span>
               </div>
               <div>
-                <span>{result.journalTitle},</span>
+                <span className="single-result__title">{result.journalTitle},</span>
                 <span>
-                  {result.journalVolume}({result.issue}):{result.pageInfo},
+                  {result.journalVolume || result.issue || result.pageInfo
+                    ? `${result.journalVolume} (${result.issue}):${result.pageInfo},`
+                    : ''}
                 </span>
                 <span>{formatDate(result.firstPublicationDate)}</span>
               </div>
@@ -42,7 +32,7 @@ const Results = ({ results }) => {
                 Cited by: {result.citedByCount} articles | PMID: {result.pmid}
                 {result.pmcid ? `| PMCID: ${result.pmcid}` : ''}
               </span>
-              <br />
+              <hr />
             </div>
           ))
         ) : (
