6ac10f6bb67c9fe83eab420b303d1d1b88f97d03
Tolani <tolani@Administrators-MacBook-Pro.local>
2022-12-02 14:36:49 +0100

    added one test case

--- src/__Tests_/result.test.js
+++ src/__Tests_/result.test.js
@@ -4,32 +4,71 @@ import axios from "axios";
 
 jest.mock("axios");
 
-const dummyTodos = [
+const dummyResults = [
 {
-  userId: 1,
-  id: 1,
-  title: "todo 1",
-  completed: false,
+  authorString: "Terwee CB, Roorda LD.",
+  citedByCount: 0,
+  doi: "10.1080/07853890.2022.2149849",
+  firstIndexDate: "2022-11-26",
+  firstPublicationDate: "2023-12-01",
+  id: "36426680",
+  issue: "1",
+  journalIssn: "0785-3890; 1365-2060; ",
+  journalTitle: "Ann Med",
+  journalVolume: "55",
+  pageInfo: "1-11",
+  pmcid: "PMC9704075",
+  pmid: "36426680",
+  pubType: "research-article; journal article",
+  pubYear: "2023",
+  source: "MED",
+  title: "Country-specific reference values for PROMIS<sup>®</sup> pain"
 },
 {
-  userId: 1,
-  id: 2,
-  title: "todo 2",
-  completed: false,
+  authorString: "Shi G, Feng J, Jian LY, Fan XY.",
+  citedByCount: 3,
+  doi: "10.4103/1673-5374.353494",
+  firstIndexDate: "2022-10-07",
+  firstPublicationDate: "2023-04-01",
+  id: "36204855",
+  issue: "1",
+  journalIssn: "1673-5374; 1876-7958; ",
+  journalTitle: "Neural Regen Res",
+  journalVolume: "18",
+  pageInfo: "863-868",
+  pmcid: "PMC9700107",
+  pmid: "36204855",
+  pubType: "research-article; journal article",
+  pubYear: "2023",
+  source: "MED",
+  title: "DNA hypomethylation promotes learning and memory recovery in a rat model of cerebral ischemia/reperfusion injury."
 },
 {
-  userId: 1,
-  id: 3,
-  title: "todo 3",
-  completed: false,
+  authorString: "Terwee CB, Roorda LD.",
+  citedByCount: 0,
+  doi: "10.1080/07853890.2022.2149849",
+  firstIndexDate: "2022-11-26",
+  firstPublicationDate: "2023-12-01",
+  id: "36426680",
+  issue: "1",
+  journalIssn: "0785-3890; 1365-2060; ",
+  journalTitle: "Ann Med",
+  journalVolume: "55",
+  pageInfo: "1-11",
+  pmcid: "PMC9704075",
+  pmid: "36426680",
+  pubType: "research-article; journal article",
+  pubYear: "2023",
+  source: "MED",
+  title: "Country-specific reference values for PROMIS<sup>®</sup> pain"
 },
 ];
 
-test("todos list", async () => {
-  axios.get.mockResolvedValue({ data: dummyTodos });
+test("results list", async () => {
+  axios.get.mockResolvedValue({ data: dummyResults });
   render(<Results />);
 
-  const todoList = await waitFor(() => screen.findAllByTestId("result"));
+  const resultList = await waitFor(() => screen.findAllByTestId("result"));
 
-  expect(todoList).toHaveLength(3);
+  expect(resultList).toHaveLength(3);
 });
